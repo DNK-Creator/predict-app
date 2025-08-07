0fef31ab21ea36877b391b689b21702af08abcd7
DNK-Creator <korotaevnikita73@gmail.com>
2025-08-07 18:18:10 +0300

    Changes to stuff

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,14 +6,14 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
-      },
-    }
-  },
+  // server: {
+  //   proxy: {
+  //     '/api': {
+  //       target: 'http://localhost:8000',
+  //       changeOrigin: true,
+  //     },
+  //   }
+  // },
   plugins: [
     vue(),
     nodePolyfills(),
