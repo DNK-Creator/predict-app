fd0a9ebed89eedab9bc419274779ebd1e82b7ef9
DNK-Creator <korotaevnikita73@gmail.com>
2025-08-07 17:07:48 +0300

    Supabase URL Fix #2

--- frontend/src/services/supabase.js
+++ frontend/src/services/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = process.env.VITE_SUPABASE_URL
-const SUPABASE_API_KEY = process.env.VITE_SUPABASE_API_KEY
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL
+const SUPABASE_API_KEY = import.meta.env.VITE_SUPABASE_API_KEY
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_API_KEY)
 export default supabase
